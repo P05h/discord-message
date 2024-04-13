f71e94205fc194ab8b57a7e61df5b5ebb57ea187
P05h <121305788+P05h@users.noreply.github.com>
2024-04-13 11:47:22 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+const url = 'https://discord.com/api/v9/channels/933182459208683530/messages?before=1227436866819854357&limit=5000';
+const token = 'your-authorization-token'; // Replace with your actual token
+
+fetch(url, {
+  headers: {
+    Authorization: `${token}`,
+  },
+})
+  .then((response) => response.text())
+  .then((data) => {
+    console.log('Fetch Response:', data);
+  })
+  .catch((error) => {
+    console.error('Fetch Error:', error.message);
+  });
